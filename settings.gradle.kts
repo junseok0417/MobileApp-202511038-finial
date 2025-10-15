3f67ad372d7378bff99752ea46fbb15e33190729
junse <junseokwon0417@gmail.com>
2025-10-15 14:33:17 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "last"
+include(":app")
+include(":app:w03")
+include(":app:test")
+include(":test2")
+include(":w03")
+include(":w04")
+include(":w05")
